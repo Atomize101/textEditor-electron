c751c6d366f5e5513a588862ff24d471fbb62c82
Chris Patrick <atomize101@gmail.com>
2020-10-20 11:14:38 -0400

    Creating the file menu

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
+require('electron-reload')(__dirname);
+
+const template = [
+	{
+		label: 'File',
+		submenu: [
+			{
+				label: 'Open File',
+				click: () => {
+					console.log('file opened');
+				},
+			},
+		],
+	},
+];
+
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
 
 function createWindow() {
 	const win = new BrowserWindow({
@@ -14,3 +32,9 @@ function createWindow() {
 }
 
 app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
